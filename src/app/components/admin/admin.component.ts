f8109299f76a3d02affb4ba7ef68cbb44b5520e6
nitishtrivedi <nitish_trivedi0194@yahoo.com>
2025-04-25 21:11:44 +0530

    Merging latest changes

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/UserModel';
+import { combineLatest, Subscription } from 'rxjs';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-admin',
   imports: [],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrl: './admin.component.css',
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit, OnDestroy {
+  userId: number | undefined;
+  user: User | undefined;
+  private subscriptions: Subscription[] = [];
+  /**
+   *
+   */
+  constructor(
+    private userService: UserService,
+    private auth: AuthenticationService
+  ) {}
 
+  ngOnInit(): void {
+    this.subscriptions.push(
+      combineLatest([this.auth.userId$]).subscribe(([uid]) => {
+        if (uid) {
+          this.getUserDetails(uid);
+        }
+      })
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  getUserDetails(id: number) {
+    this.userService.getUser(id).subscribe({
+      next: (data) => {
+        this.user = data;
+      },
+    });
+  }
 }
